fbed6f73d02c8f380faf4cc30d1f817fe4e5ba9d
Hana Quadara <hana.quadara@gmail.com>
2019-12-05 21:44:32 -0500

    Work in progress

--- app/src/views/MainView.js
+++ app/src/views/MainView.js
@@ -1,8 +1,146 @@
 import React from "react";
+import styled from "styled-components";
 import Quagga from "quagga";
 
-const Quagga = require("quagga").default;
+var _scannerIsRunning = false;
 
-const MainView = () => <p>Sup bitches</p>;
+const startScanner = () => {
+  Quagga.init(
+    {
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector("#scanner-container"),
+        constraints: {
+          width: 480,
+          height: 320,
+          facingMode: "environment"
+        }
+      },
+      decoder: {
+        readers: [
+          "code_128_reader",
+          "ean_reader",
+          "ean_8_reader",
+          "code_39_reader",
+          "code_39_vin_reader",
+          "codabar_reader",
+          "upc_reader",
+          "upc_e_reader",
+          "i2of5_reader"
+        ],
+        debug: {
+          showCanvas: true,
+          showPatches: true,
+          showFoundPatches: true,
+          showSkeleton: true,
+          showLabels: true,
+          showPatchLabels: true,
+          showRemainingPatchLabels: true,
+          boxFromPatches: {
+            showTransformed: true,
+            showTransformedBox: true,
+            showBB: true
+          }
+        }
+      }
+    },
+    function(err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
 
+      // Set flag to is running
+      _scannerIsRunning = true;
+    }
+  );
+  Quagga.onProcessed(function(result) {
+    var drawingCtx = Quagga.canvas.ctx.overlay,
+      drawingCanvas = Quagga.canvas.dom.overlay;
+
+    if (result) {
+      if (result.boxes) {
+        drawingCtx.clearRect(
+          0,
+          0,
+          parseInt(drawingCanvas.getAttribute("width")),
+          parseInt(drawingCanvas.getAttribute("height"))
+        );
+        result.boxes
+          .filter(function(box) {
+            return box !== result.box;
+          })
+          .forEach(function(box) {
+            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+              color: "green",
+              lineWidth: 2
+            });
+          });
+      }
+
+      if (result.box) {
+        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+          color: "#00F",
+          lineWidth: 2
+        });
+      }
+
+      if (result.codeResult && result.codeResult.code) {
+        Quagga.ImageDebug.drawPath(
+          result.line,
+          { x: "x", y: "y" },
+          drawingCtx,
+          { color: "red", lineWidth: 3 }
+        );
+      }
+    }
+  });
+
+  Quagga.onDetected(function(result) {
+    console.log(
+      "Barcode detected and processed : [" + result.codeResult.code + "]",
+      result
+    );
+  });
+};
+
+const Button = styled.div`
+  width: 200px;
+  height: 50px;
+  background-color: cyan;
+  display: flex;
+  vertical-align: middle;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ButtonStop = styled.div`
+  width: 200px;
+  height: 50px;
+  background-color: red;
+  display: flex;
+  vertical-align: middle;
+  align-items: center;
+  justify-content: center;
+`;
+
+const handleClick = () => {
+  console.log("Clicked");
+  startScanner();
+};
+
+const MainView = () => {
+  console.log(_scannerIsRunning);
+  return (
+    <React.Fragment>
+      <p>Suppp</p>
+      <div id="scanner-container" />
+      <Button onClick={() => handleClick()}>Start Scanner</Button>
+      <ButtonStop onClick={() => Quagga.stop()}>Stop Scanner</ButtonStop>
+    </React.Fragment>
+  );
+};
 export default MainView;
