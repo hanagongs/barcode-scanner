2aa0c319873a7521c67b7cce1e130133fc3ea475
Hana Quadara <hana.quadara@gmail.com>
2019-12-05 20:40:05 -0500

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MainView from "./views";
+
+function App() {
+  return <MainView />;
+}
+
+export default App;
---
+++ app/src/views/MainView.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Quagga from "quagga";
+
+const Quagga = require("quagga").default;
+
+const MainView = () => <p>Sup bitches</p>;
+
+export default MainView;
